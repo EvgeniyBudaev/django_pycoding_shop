8794b8a54584d1809c84a3506d9dcb5312e9d1ae
EvgeniyBudaev <49036840+EvgeniyBudaev@users.noreply.github.com>
2021-03-13 09:15:14 +0300

    fix models & Применение миграций приложения и related_name

--- mainapp/models.py
+++ mainapp/models.py
@@ -31,7 +31,7 @@ class Product(models.Model):
   slug = models.SlugField(unique=True)
   image = models.ImageField(verbose_name='Изображение')
   description = models.TextField(verbose_name='Описание', null=True)  # null=True - поле может быть пустым
-  price = models.DecimalField(max_length=9, decimal_places=2, verbose_name='Цена')  # decimal_places - количество цифр после запятой
+  price = models.DecimalField(max_digits=9, decimal_places=2, verbose_name='Цена')  # decimal_places - количество цифр после запятой
 
   def __str__(self):
     return self.title
@@ -39,10 +39,10 @@ class Product(models.Model):
 
 class CartProduct(models.Model):
   user = models.ForeignKey('Customer', verbose_name='Покупатель', on_delete=models.CASCADE)  # 'Customer' в строке т.к. модель Customer не объявлена ранее
-  cart = models.ForeignKey('Cart', verbose_name='Корзина', on_delete=models.CASCADE)
+  cart = models.ForeignKey('Cart', verbose_name='Корзина', on_delete=models.CASCADE,  related_name='related_products')  # Мы хотим узнать к какой корзине cartproduct относиться.  cartproduct.related_cart.метод()
   product = models.ForeignKey(Product , verbose_name='Товар', on_delete=models.CASCADE)
   qty = models.PositiveIntegerField(default=1)  # PositiveIntegerField - models.PositiveIntegerField
-  final_price = models.DecimalField(max_length=9, decimal_places=2, verbose_name='Общая цена')
+  final_price = models.DecimalField(max_digits=9, decimal_places=2, verbose_name='Общая цена')
 
   def __str__(self):
     return f'Продукт {self.product.title} (для корзины)'
@@ -50,9 +50,9 @@ class CartProduct(models.Model):
 
 class Cart(models.Model):
   owner = models.ForeignKey('Customer', verbose_name='Владелец', on_delete=models.CASCADE)  # owner - владелец
-  products = models.ManyToManyField(CartProduct, blank=True)  # blank=True - данное поле не обязательно к заполнению
+  products = models.ManyToManyField(CartProduct, blank=True, related_name='related_cart')  # blank=True - данное поле не обязательно к заполнению. cart.related_products.all() получаем querySet со всеми продуктами, которые в данный момент находятся в данной корзине
   total_products = models.PositiveIntegerField(default=0)
-  final_price = models.DecimalField(max_length=9, decimal_places=2, verbose_name='Общая цена')
+  final_price = models.DecimalField(max_digits=9, decimal_places=2, verbose_name='Общая цена')
 
   def __str__(self):
     return str(self.id)
